bea409e3bd18214f58a657bc6a9e5205bbfbc8d2
KareemAbdelaty <69083495+KareemAbdelaty@users.noreply.github.com>
2022-11-14 00:33:26 -0700

    Steps Page

---
+++ CookingBuddy/Pages/LetsCook/model/LetsCookModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookingBuddy.Pages.LetsCook.model
+{
+
+    public class LetsCookModel
+    {
+        public string videourl;
+        public (string, string)[] StepDescritpionPair;
+        public LetsCookModel(string url, (string, string)[] steps)
+        {
+            this.videourl = url;
+            this.StepDescritpionPair = steps;
+        }
+
+    }
+}
