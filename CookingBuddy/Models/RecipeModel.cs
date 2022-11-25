695aa062992afaa57fe556a0df9081f798317928
andrewBurrr <andrewburton345@gmail.com>
2022-11-24 20:28:40 -0700

    Added SQL setup and project models (database not yet configured)

---
+++ CookingBuddy/Models/RecipeModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using SQLite;
+
+namespace CookingBuddy.Models
+{
+    public class Recipe
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Media { get; set; }
+        public float Price { get; set; }
+        public int Yield { get; set; }
+        public int Time { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class Ingredient
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Stage
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+    }
+
+    public class Step
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int StageId { get; set; }
+        public string Content { get; set; }
+    }
+}
+
---
+++ CookingBuddy/Services/LessonService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CookingBuddy.Models;
+namespace CookingBuddy.Services
+{
+    public class LessonService
+    {
+        private static readonly string[] Titles = new[]
+        {
+            "sample title", "other sample"
+        };
+        public Task<LessonModel[]> GetLessonAsync()
+        {
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new LessonModel
+            {
+                // create your data
+                Title = Titles[Random.Shared.Next(Titles.Length)],
+                Author = "",
+                Rating = 0.0F,
+                Image = "http://",
+                Instructions = new List<string> { "do a thing", "do a different thing", "do last thing" }
+            }).ToArray());
+        }
+    }
+}
+
